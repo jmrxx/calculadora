5c8047bf88d6aed04fcd17e7dcf220f561fa0a50
Jose Mejia <86321536+jmrxx@users.noreply.github.com>
2022-08-21 05:24:13 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+/* JMRX - JOSE MEJIA" */
+
+// We select the id of the div where the results will be seen
+let screen = document.getElementById("screen");
+
+/* We convert the buttons into an array, taking the class of the buttons */
+let buttons = Array.from(document.getElementsByClassName("button"));
+
+/* we map the array data to add the click event,
+with the switch we do the operations according to the case */
+buttons.map((button) => {
+  button.addEventListener("click", (e) => {
+    switch (e.target.innerText) {
+      case "C":
+        screen.innerText = "";
+        break;
+      case "←":
+        if (screen.innerText) {
+          screen.innerText = screen.innerText.slice(0, -1);
+        }
+        break;
+      case "=":
+        try {
+          screen.innerText = eval(screen.innerText);
+        } catch (error) {
+          screen.innerText = "Error";
+        }
+        
+        break;
+      default:
+        screen.innerText += e.target.innerText;
+    }
+  });
+});
